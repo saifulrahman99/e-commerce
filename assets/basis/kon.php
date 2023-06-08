614e99c876a4a138040957746fcad096331b48eb
Saiful Rahman <sr.perante@gmail.com>
2023-06-08 21:02:58 +0700

    file hosting

--- assets/basis/kon.php
+++ assets/basis/kon.php
@@ -1,2 +1,3 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "adastra");
+//$db = mysqli_connect("localhost", "root", "", "adastra");
+$db = mysqli_connect("localhost", "saiz5787_yusuf", "yusuf5787", "saiz5787_yusuf");
